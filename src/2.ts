63819f314c81068a063c3f8398e8368b14e8a8ef
hoskinbasil42 <hoskinbasil42@gmail.com>
2025-03-07 02:12:54 +0000

    Add 2.ts

---
+++ src/2.ts
@@ -0,0 +1,16 @@
+// Proj1.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proj1',
+  templateUrl: './proj1.component.html',
+  styleUrls: ['./proj1.component.css']
+})
+export class Proj1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
