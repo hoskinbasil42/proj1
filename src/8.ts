271625c2ff0f77d96531b29cd1350e827b3562c6
hoskinbasil42 <hoskinbasil42@gmail.com>
2025-03-14 10:50:50 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proj1',
+  templateUrl: './proj1.component.html',
+  styleUrls: ['./proj1.component.css']
+})
+export class Proj1Component {
+  title = 'Project 1';
+}
